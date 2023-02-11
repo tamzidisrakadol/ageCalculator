9827198dc87fa609dff63b086d97e32f3d5bbaba
Tamzid Israk <tamzidisrak12@gmail.com>
2023-02-12 02:25:12 +0600

    Calculate your age into minutes add layout design of activity_main.xml. user select the date from calendar and tap calculate button and the selected date will convert into minutes by subtracting from current date to selected date

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -1,11 +1,87 @@
 package com.example.agecalculator
 
+import android.app.DatePickerDialog
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var selectDateTV: TextView
+    private lateinit var convertIntoTime: TextView
+    private lateinit var calculateBtn: Button
+    private lateinit var dateToSdf: Date
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectDateTV = findViewById(R.id.dobTv)
+        convertIntoTime = findViewById(R.id.timeTV)
+        calculateBtn = findViewById(R.id.calculateBtn)
+
+        selectDateTV.setOnClickListener {
+            pickUpDate(it)
+        }
+
+        calculateBtn.setOnClickListener {
+            convertDateToTime()
+        }
+
+    }
+
+
+    private fun pickUpDate(view: View) {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+                selectDateTV.text = selectedDate
+
+                var sdf = SimpleDateFormat("dd/MM/yyy", Locale.ENGLISH)
+                dateToSdf = sdf.parse(selectedDate)
+
+
+            },
+            year,
+            month,
+            day
+        )
+
+        //setting the max date in Calendar
+        dpd.datePicker.maxDate = Date().time - 86400000
+        dpd.show()
+
+    }
+
+
+    private fun convertDateToTime() {
+        var sdf = SimpleDateFormat("dd/MM/yyy", Locale.ENGLISH)
+
+        //convert the selected date into Minutes
+        val selectedDateIntoMin = dateToSdf!!.time / 60000
+
+        //select the current date
+        val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+        //convert the current date into minute
+        val currentDateIntoMinute = currentDate!!.time / 60000
+
+        //subtract from current date to selectDate
+        val differenceTime = currentDateIntoMinute - selectedDateIntoMin
+
+        convertIntoTime.text = differenceTime.toString()
+        convertIntoTime.setBackgroundColor(Color.GRAY)
     }
 }
